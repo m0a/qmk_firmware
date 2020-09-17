7ca86cc5c6a55f8c29cae863594df01de3fe51a3
m0a <abe00makoto@gmail.com>
2020-09-18 01:13:20 +0900

    fix _RAISE

--- keyboards/ergodash/rev1/keymaps/m0a/keymap.c
+++ keyboards/ergodash/rev1/keymaps/m0a/keymap.c
@@ -36,19 +36,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,  KC_NO,  KC_UP,   KC_NO,    KC_NO,    KC_NO,                     KC_PPLS,  KC_EQL,  KC_LCBR, KC_RCBR, KC_COLN, KC_DQT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_LEFT, KC_DOWN,KC_RIGHT,  KC_NO,    KC_NO,                     KC_PIPE, KC_GRV, KC_COMM,  KC_LBRC, KC_RBRC, KC_QUES,\
+      KC_LSFT, KC_LEFT, KC_DOWN,KC_RIGHT,  KC_NO,    KC_NO,                     KC_PIPE,  KC_GRV,  KC_LBRC, KC_RBRC, KC_QUES, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_TRNS,   KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS \
                                       //`--------------------------'  `--------------------------'
   ),
 
 
-  [_RAISE] = LAYOUT_4key_2u_inner( \
-    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LPRN,                            KC_RPRN, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   \
-    KC_NO,   KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,               KC_TRNS, KC_TRNS,                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   \
-    KC_NO,   KC_NO,   KC_NO,         KC_NO,        KC_NO,   KC_NO,    KC_NO,   KC_NO,     KC_NO,  KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO    \
+  [_RAISE] = LAYOUT_CRKBD_LIKE( \
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,  KC_NO,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,                        KC_F11, KC_F12,  KC_NO,   KC_NO,    KC_NO,  KC_RSFT,\
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        KC_TRNS,   KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS \
+                                      //`--------------------------'  `--------------------------'
   ),
 
 
